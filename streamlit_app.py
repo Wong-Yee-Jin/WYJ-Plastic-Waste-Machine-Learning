507716de70b5d19538003d6e33985d55d4eca79b
Wong-Yee-Jin <1009320@mymail.sutd.edu.sg>
2025-08-29 17:55:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('♻ Singapore Plastic Waste Disposed Machine Learning')
+
+st.info('This is a machine learning model.')
 
 st.write('Hello world!')
