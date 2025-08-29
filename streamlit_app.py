aaccb92ff3d35211581500cfdde7f8ccf9257afb
Wong-Yee-Jin <1009320@mymail.sutd.edu.sg>
2025-08-29 19:42:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 st.title('♻ Singapore Plastic Waste Disposed Machine Learning')
 
 st.info('This is a machine learning model.')
 
 with st.expander('Raw Data'):
-  st.write('**Description of raw data...**\nHello')
+  st.write('**Description of raw data...**')
   df = pd.read_csv('SG_Plastic_Waste_GDP_Population_Dataset.csv')
   df
