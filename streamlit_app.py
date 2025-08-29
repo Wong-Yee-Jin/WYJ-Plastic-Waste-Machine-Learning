cd8f58ebad4009d28f5df177642022265e005bc5
Wong-Yee-Jin <1009320@mymail.sutd.edu.sg>
2025-08-29 23:05:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,4 +81,4 @@ output_predict: np.ndarray = predict_linreg(input_df, model['beta'], model['mean
 
 st.header('Predicted Volume of Plastic Waste Disposed in Singapore')
 st.table(output_df)
-st.success(f'Predicted Plastic Waste Disposed in Singapore: {output_predict}')
+st.success(f'Predicted Plastic Waste Disposed in Singapore: {output_predict[0][0]:.4f}')
