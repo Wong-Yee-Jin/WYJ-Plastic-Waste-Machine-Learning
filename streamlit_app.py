ef45c53331a0230ace01456b288494e788f176e2
Wong-Yee-Jin <1009320@mymail.sutd.edu.sg>
2025-08-29 19:50:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import pandas as pd
 
+independent_variables = ['Year', 'Total SG Population', 'SG GDP Per Capita']
+dependent_variable = ['Plastic Waste Disposed (Tonnes)']
+
 st.set_page_config(layout="wide")
-
 st.title('♻ Singapore Plastic Waste Disposed Machine Learning')
-
 st.info('This is a machine learning model.')
 
+st.write('Describe how the data is obtained...')
+
 with st.expander('Raw Data'):
-  st.write('**Description of raw data...**')
   df = pd.read_csv('SG_Plastic_Waste_GDP_Population_Dataset.csv')
   df
+
+st.write('Describe the independent and dependent variables...')
+
+with st.expander('Independent Variables'):
+  X = df.drop(dependent_variable)
+  X
+
+with st.expander('Dependent Variable'):
+  y = df.drop(columns=independent_variables)
+  y
