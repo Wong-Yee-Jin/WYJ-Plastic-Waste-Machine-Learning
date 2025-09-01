beffe7b7b7093c74d8d018ab9d164c12a9aff7d1
Wong-Yee-Jin <1009320@mymail.sutd.edu.sg>
2025-09-01 13:16:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,38 @@ citation_links = ['Singapore Department of Statistics. (2024). Waste Management 
                  'Envcares. (2025). Plastics wastes in Singapore. https://envcares.com.sg/plastics-wastes-in-singapore/']
 
 
+def predict_plastic_waste(sg_population, sg_gdp, indep_variables, dep_variable):
+  # PREPARE FEATURES & TARGET SETS
+  input_data = {'Total SG Population': sg_population, 'SG GDP Per Capita': sg_gdp}
+  input_df = pd.DataFrame(input_data, index=[0])
+  df_features, df_target = get_features_targets(df, indep_variables, dep_variable)
+  df_features_train, df_features_test, df_target_train, df_target_test = split_data(df_features, df_target, 100, 0.2)
+  array_features,_,_ = normalize_z(df_features.to_numpy())
+  X: np.ndarray = prepare_feature(array_features)
+  # BUILD & TEST LINEAR REGRESSION MODEL
+  target: np.ndarray = df_target.to_numpy()
+  iterations: int = 1500
+  alpha: float = 0.01
+  beta: np.ndarray = np.zeros((X.shape[1], 1))
+  beta, J_storage = gradient_descent_linreg(X, target, beta, alpha, iterations)
+  pred: np.ndarray = predict_linreg(df_features, beta)
+  target: np.ndarray = df_target.to_numpy()
+  model, J_storage = build_model_linreg(df_features_train, df_target_train, beta, alpha, iterations)
+  pred: np.ndarray = predict_linreg(df_features_test, model['beta'], model['means'], model['stds'])
+  # EVALUATE LINEAR REGRESSION MODEL
+  target: np.ndarray = df_target_test.to_numpy()
+  mse: float = mean_squared_error(target, pred)
+  rmse = np.sqrt(mse)
+  abs_errors = np.abs(target - pred)
+  mean_abs_error = np.mean(abs_errors)
+  output_data = {'Mean Squared Error (MSE)': f'{mse:,.4f}', 'Root Mean Squared Error (RMSE)': f'{rmse:,.4f}', 'Mean Absolute Error (MAE)': f'{mean_abs_error:,.4f}'}
+  output_df = pd.DataFrame(output_data, index=[0])
+  # PREDICT USING INPUT FEATURES FROM USER
+  output_predict: np.ndarray = predict_linreg(input_df, model['beta'], model['means'], model['stds'])
+  prediction_val = round(output_predict[0][0])
+  return prediction_val
+
+
 st.set_page_config(page_title="SG Plastic Waste Disposed", page_icon=":recycle:", layout="wide")
 st.title(':recycle: Singapore Plastic Waste Disposed Machine Learning')
 st.info("Problem Statement: using **supervised learning** (Multiple Linear Regression), how might we **predict the volume of plastic waste disposed in Singapore** based on population size and GDP per capita to aid the Singapore Government's decision-making to promote sustainable growth and effective waste management?")
@@ -21,7 +53,45 @@ if 'state' not in st.session_state or st.session_state['state'] is None:
   st.session_state['state'] = 1
 if 'new state' not in st.session_state:
   st.session_state['new state'] = 1
-  
+
+
+st.header('Predict the Volume of Plastic Waste Disposed in Singapore')
+st.write('Root Mean Squared Error (RMSE) represents the average magnitude of the error between the predicted and actual values, with greater weight given to larger errors. In the context of national-level policy, such a margin of error is considered acceptable due to the scale of the data involved.')
+st.write('Mean Absolute Error (MAE) reflects the average absolute difference between predicted and actual values. An error of around 70,000 tonnes indicates that the model produces reasonably close estimates across the dataset.')
+st.table(output_df)
+col5, col6 = st.columns(2)
+with col5:
+  st.write('**[Activity :wave:] Select the following population and GDP per capita in Singapore:**')
+  sg_population = st.slider('Singapore Population', 3000000, 1000000000, 5000000)
+  sg_gdp = st.slider('Singapore GDP Per Capita', 30000, 1000000000, 500000)
+  input_display = pd.DataFrame({'Total SG Population': f'{sg_population:,}', 'SG GDP Per Capita': f'{sg_gdp:,}'}, index=[0])
+  input_display
+  prediction_val = predict_plastic_waste(sg_population, sg_gdp, independent_variables, dependent_variable)
+  prediction_msg = f'Predicted Volume of Plastic Waste Disposed in Singapore: **{prediction_val:,} tonnes**'
+  if prediction_val < 2000000000:
+    st.success(prediction_msg)
+  elif 2000000000 <= prediction_val <= 6000000000:
+    st.warning(prediction_msg)
+  elif prediction_val > 6000000000:
+    st.error(prediction_msg)
+with col6:
+  if prediction_val < 1000000000:
+    st.session_state['new state'] = 1
+  elif 1000000000 <= prediction_val <= 2000000000:
+    st.session_state['new state'] = 2
+  elif 2000000000 <= prediction_val <= 4000000000:
+    st.session_state['new state'] = 3
+  elif 4000000000 <= prediction_val <= 6000000000:
+    st.session_state['new state'] = 4
+  elif prediction_val > 6000000000:
+    st.session_state['new state'] = 5
+  if st.session_state['state'] != st.session_state['new state']:
+    file_path = f"animation/{st.session_state['state']}_{st.session_state['new state']}.gif"
+  else:
+    file_path = f"animation/{st.session_state['new state']}.png"
+  st.image(file_path)
+  st.session_state['state'] = st.session_state['new state']
+
 
 # DATA EXTRACTION
 st.header('Data Extraction')
@@ -73,41 +143,30 @@ with st.sidebar:
 # input_display
 
 
-# PREPARE FEATURES & TARGET SETS
-# df[dependent_variable[0]] = np.log(df[dependent_variable[0]]) # logarithm y variable
-df_features, df_target = get_features_targets(df, independent_variables, dependent_variable)
-df_features_train, df_features_test, df_target_train, df_target_test = split_data(df_features, df_target, 100, 0.2)
-array_features,_,_ = normalize_z(df_features.to_numpy())
-X: np.ndarray = prepare_feature(array_features)
+# # PREPARE FEATURES & TARGET SETS
+# input_data = {'Total SG Population': sg_population, 'SG GDP Per Capita': sg_gdp}
+# input_df = pd.DataFrame(input_data, index=[0])
+# input_display = pd.DataFrame({'Total SG Population': f'{sg_population:,}', 'SG GDP Per Capita': f'{sg_gdp:,}'}, index=[0])
+# df_features, df_target = get_features_targets(df, independent_variables, dependent_variable)
+# df_features_train, df_features_test, df_target_train, df_target_test = split_data(df_features, df_target, 100, 0.2)
+# array_features,_,_ = normalize_z(df_features.to_numpy())
+# X: np.ndarray = prepare_feature(array_features)
 
 
-# BUILD & TEST LINEAR REGRESSION MODEL
-target: np.ndarray = df_target.to_numpy()
-iterations: int = 1500
-alpha: float = 0.01
-beta: np.ndarray = np.zeros((X.shape[1], 1))
-beta, J_storage = gradient_descent_linreg(X, target, beta, alpha, iterations)
-pred: np.ndarray = predict_linreg(df_features, beta)
-target: np.ndarray = df_target.to_numpy()
-model, J_storage = build_model_linreg(df_features_train, df_target_train, beta, alpha, iterations)
-pred: np.ndarray = predict_linreg(df_features_test, model['beta'], model['means'], model['stds'])
+# # BUILD & TEST LINEAR REGRESSION MODEL
+# target: np.ndarray = df_target.to_numpy()
+# iterations: int = 1500
+# alpha: float = 0.01
+# beta: np.ndarray = np.zeros((X.shape[1], 1))
+# beta, J_storage = gradient_descent_linreg(X, target, beta, alpha, iterations)
+# pred: np.ndarray = predict_linreg(df_features, beta)
+# target: np.ndarray = df_target.to_numpy()
+# model, J_storage = build_model_linreg(df_features_train, df_target_train, beta, alpha, iterations)
+# pred: np.ndarray = predict_linreg(df_features_test, model['beta'], model['means'], model['stds'])
 
 
-# EVALUATE LINEAR REGRESSION MODEL
-target: np.ndarray = df_target_test.to_numpy()
-mse: float = mean_squared_error(target, pred)
-rmse = np.sqrt(mse)
-abs_errors = np.abs(target - pred)
-mean_abs_error = np.mean(abs_errors)
-output_data = {'Mean Squared Error (MSE)': f'{mse:,.4f}', 'Root Mean Squared Error (RMSE)': f'{rmse:,.4f}', 'Mean Absolute Error (MAE)': f'{mean_abs_error:,.4f}'}
-output_df = pd.DataFrame(output_data, index=[0])
-
-
-# BUILD + TEST + EVALUATE LINEAR REGRESSION MODEL (LOGARITHM)
-# pred_log: np.ndarray = predict_linreg(df_features_test, model['beta'], model['means'], model['stds'])
-# pred: np.ndarray = np.exp(pred_log)
-# target_log: np.ndarray = df_target_test.to_numpy()
-# target: np.ndarray = np.exp(target_log)
+# # EVALUATE LINEAR REGRESSION MODEL
+# target: np.ndarray = df_target_test.to_numpy()
 # mse: float = mean_squared_error(target, pred)
 # rmse = np.sqrt(mse)
 # abs_errors = np.abs(target - pred)
@@ -116,50 +175,48 @@ output_df = pd.DataFrame(output_data, index=[0])
 # output_df = pd.DataFrame(output_data, index=[0])
 
 
-# PREDICT USING INPUT FEATURES FROM USER
-output_predict: np.ndarray = predict_linreg(input_df, model['beta'], model['means'], model['stds'])
-prediction_val = round(output_predict[0][0])
+# # PREDICT USING INPUT FEATURES FROM USER
+# output_predict: np.ndarray = predict_linreg(input_df, model['beta'], model['means'], model['stds'])
+# prediction_val = round(output_predict[0][0])
 
 
-st.header('Predict the Volume of Plastic Waste Disposed in Singapore')
-st.write('Root Mean Squared Error (RMSE) represents the average magnitude of the error between the predicted and actual values, with greater weight given to larger errors. In the context of national-level policy, such a margin of error is considered acceptable due to the scale of the data involved.')
-st.write('Mean Absolute Error (MAE) reflects the average absolute difference between predicted and actual values. An error of around 70,000 tonnes indicates that the model produces reasonably close estimates across the dataset.')
-st.table(output_df)
-col5, col6 = st.columns(2)
-with col5:
-  # st.write('')
-  # st.write('')
-  st.write('**[Activity :wave:] Select the following population and GDP per capita in Singapore:**')
-  sg_population = st.slider('Singapore Population', 3000000, 1000000000, 5000000)
-  sg_gdp = st.slider('Singapore GDP Per Capita', 30000, 1000000000, 500000)
-  input_data = {'Total SG Population': sg_population, 'SG GDP Per Capita': sg_gdp}
-  input_df = pd.DataFrame(input_data, index=[0])
-  input_display = pd.DataFrame({'Total SG Population': f'{sg_population:,}', 'SG GDP Per Capita': f'{sg_gdp:,}'}, index=[0])
-  input_display
-  prediction_msg = f'Predicted Volume of Plastic Waste Disposed in Singapore: **{prediction_val:,} tonnes**'
-  if prediction_val < 2000000000:
-    st.success(prediction_msg)
-  elif 2000000000 <= prediction_val <= 6000000000:
-    st.warning(prediction_msg)
-  elif prediction_val > 6000000000:
-    st.error(prediction_msg)
-with col6:
-  if prediction_val < 1000000000:
-    st.session_state['new state'] = 1
-  elif 1000000000 <= prediction_val <= 2000000000:
-    st.session_state['new state'] = 2
-  elif 2000000000 <= prediction_val <= 4000000000:
-    st.session_state['new state'] = 3
-  elif 4000000000 <= prediction_val <= 6000000000:
-    st.session_state['new state'] = 4
-  elif prediction_val > 6000000000:
-    st.session_state['new state'] = 5
-  if st.session_state['state'] != st.session_state['new state']:
-    file_path = f"animation/{st.session_state['state']}_{st.session_state['new state']}.gif"
-  else:
-    file_path = f"animation/{st.session_state['new state']}.png"
-  st.image(file_path)
-  st.session_state['state'] = st.session_state['new state']
+# st.header('Predict the Volume of Plastic Waste Disposed in Singapore')
+# st.write('Root Mean Squared Error (RMSE) represents the average magnitude of the error between the predicted and actual values, with greater weight given to larger errors. In the context of national-level policy, such a margin of error is considered acceptable due to the scale of the data involved.')
+# st.write('Mean Absolute Error (MAE) reflects the average absolute difference between predicted and actual values. An error of around 70,000 tonnes indicates that the model produces reasonably close estimates across the dataset.')
+# st.table(output_df)
+# col5, col6 = st.columns(2)
+# with col5:
+#   st.write('**[Activity :wave:] Select the following population and GDP per capita in Singapore:**')
+#   sg_population = st.slider('Singapore Population', 3000000, 1000000000, 5000000)
+#   sg_gdp = st.slider('Singapore GDP Per Capita', 30000, 1000000000, 500000)
+#   # input_data = {'Total SG Population': sg_population, 'SG GDP Per Capita': sg_gdp}
+#   # input_df = pd.DataFrame(input_data, index=[0])
+#   # input_display = pd.DataFrame({'Total SG Population': f'{sg_population:,}', 'SG GDP Per Capita': f'{sg_gdp:,}'}, index=[0])
+#   input_display
+#   prediction_msg = f'Predicted Volume of Plastic Waste Disposed in Singapore: **{prediction_val:,} tonnes**'
+#   if prediction_val < 2000000000:
+#     st.success(prediction_msg)
+#   elif 2000000000 <= prediction_val <= 6000000000:
+#     st.warning(prediction_msg)
+#   elif prediction_val > 6000000000:
+#     st.error(prediction_msg)
+# with col6:
+#   if prediction_val < 1000000000:
+#     st.session_state['new state'] = 1
+#   elif 1000000000 <= prediction_val <= 2000000000:
+#     st.session_state['new state'] = 2
+#   elif 2000000000 <= prediction_val <= 4000000000:
+#     st.session_state['new state'] = 3
+#   elif 4000000000 <= prediction_val <= 6000000000:
+#     st.session_state['new state'] = 4
+#   elif prediction_val > 6000000000:
+#     st.session_state['new state'] = 5
+#   if st.session_state['state'] != st.session_state['new state']:
+#     file_path = f"animation/{st.session_state['state']}_{st.session_state['new state']}.gif"
+#   else:
+#     file_path = f"animation/{st.session_state['new state']}.png"
+#   st.image(file_path)
+#   st.session_state['state'] = st.session_state['new state']
 
 
 st.header('Discussion and Analysis of Results')
